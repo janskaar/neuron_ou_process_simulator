b53152b5c86cc24a2c8d74fff88d25b782625da0
janeirik <>
2023-08-09 12:37:35 +0200

    compare also to simulaton

--- examples/compare_schwalger_equations.py
+++ examples/compare_schwalger_equations.py
@@ -6,10 +6,10 @@ from neurosim.simulator import SimulationParameters, MomentsSimulator, MembraneP
 from neurosim.n_functions import compute_n1, pdf_b, xy_to_xv, f1_schwalger
 
 
-p1 = SimulationParameters(threshold=0.02, dt=0.01, I_e = 0., num_procs=100000, sigma_noise = 1.5, C = 1.)
-D = p1.tau_y * p1.sigma2_noise / p1.tau_x ** 2
-s_y = D / p1.tau_y
-p1.sigma_noise = np.sqrt(2 * D) / p1.tau_y
+p = SimulationParameters(threshold=0.02, dt=0.01, I_e = 0., num_procs=100000, sigma_noise = 1.5, C = 1.)
+D = p.tau_y * p.sigma2_noise / p.tau_x ** 2
+s_y = D / p.tau_y
+p.sigma_noise = np.sqrt(2 * D) / p.tau_y
 
 u_0 = 0.
 mu_0 = np.zeros(2, dtype=np.float64)
@@ -17,32 +17,36 @@ s_0 = np.zeros(3, dtype=np.float64)
 s_0[0] = s_y
 
 
-uSim = MembranePotentialSimulator(u_0, p1)
+uSim = MembranePotentialSimulator(u_0, p)
 uSim.simulate(20.)
 
 
-schwalgerSim = MomentsSimulator(mu_0, s_0, p1)
+schwalgerSim = MomentsSimulator(mu_0, s_0, p)
 schwalgerSim.simulate(20.)
-f1 = f1_schwalger(uSim.b, uSim.b_dot, schwalgerSim.mu, schwalgerSim.s, p1)
+f1 = f1_schwalger(uSim.b, uSim.b_dot, schwalgerSim.mu, schwalgerSim.s, p)
 
 
-# p2 = SimulationParameters(threshold=0.02, dt=0.01, I_e = 0., num_procs=100000, sigma_noise = 1.5, C = 1.)
-p2 = p1
 mu_0 = np.zeros(2, dtype=np.float64)
 s_0 = np.zeros(3, dtype=np.float64)
-s_0[0] = p2.sigma2_noise * p2.tau_y / 2.
+s_0[0] = p.sigma2_noise * p.tau_y / 2.
 
-momentsSim = MomentsSimulator(mu_0, s_0, p2)
+momentsSim = MomentsSimulator(mu_0, s_0, p)
 
 momentsSim.simulate(20.)
 
 
-mu, s = xy_to_xv(momentsSim.mu, momentsSim.s, p2)
+z_0 = np.zeros((p.num_procs, 2), dtype=np.float64)
+z_0[:,0] = np.random.randn(p.num_procs) * np.sqrt(s_y)
+pSim = ParticleSimulator(z_0, 0., p)
+pSim.simulate(20.)
+
+mu, s = xy_to_xv(momentsSim.mu, momentsSim.s, p)
 n1 = compute_n1(uSim.b, uSim.b_dot, mu, s)
 
 
-plt.plot(n1)
-plt.plot(f1)
+plt.plot(pSim.upcrossings.sum(1), label="simulation")
+plt.plot(n1 * p.num_procs * p.dt, '--', label="n1")
+plt.plot(f1 * p.num_procs * p.dt, '--', label="p1")
+plt.legend()
 plt.show()
-
 
